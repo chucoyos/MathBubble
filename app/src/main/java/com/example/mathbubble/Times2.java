722f0f4ed94fd99bc3b92ac780f73f46a2a1606c
chucoyos <facturasmanzanillo@hotmail.com>
2021-03-21 14:28:56 -0600

    Add rightAnswer

--- app/src/main/java/com/example/mathbubble/Times2.java
+++ app/src/main/java/com/example/mathbubble/Times2.java
@@ -2,18 +2,22 @@ package com.example.mathbubble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class Times2 extends AppCompatActivity {
 
     Button topLeftButton, topRightButton, midLeftButton, midRightButton, bottomLeftButton, bottomRightButton;
-    int wrongAnswer;
+    TextView textView;
+    int multiplying = 2;
+    int rightAnswer;
 
-    int [] answers = {1,2,3,4,5,6};
+    int [] answers = {1,2,3,4};
     Button [] buttons;
 
     @Override
@@ -27,26 +31,26 @@ public class Times2 extends AppCompatActivity {
 
         topLeftButton = findViewById(R.id.topLeftButton);
         topRightButton = findViewById(R.id.topRightButton);
-        midLeftButton = findViewById(R.id.midLeftButton);
-        midRightButton = findViewById(R.id.midRightButton);
         bottomLeftButton = findViewById(R.id.bottomLeftButton);
         bottomRightButton = findViewById(R.id.bottomRightButton);
+        textView = findViewById(R.id.textView);
         setAnswers();
     }
 
+    @SuppressLint("DefaultLocale")
     public void setAnswers(){
+        Random random = new Random();
+        int multiplier = random.nextInt(10) + 1;
+        textView.setText(String.format( "%d X %d", multiplying, multiplier));
+        rightAnswer = multiplying * multiplier;
 
         for (int i = 0; i < answers.length; i++){
-            Random random = new Random();
-            wrongAnswer = random.nextInt(10) + 1;
-                answers[i] = wrongAnswer * 2;
+                answers[i] = (random.nextInt(10) + 1) * multiplying;
 
         }
-         topLeftButton.setText(String.valueOf(answers[0]));
+         topLeftButton.setText(String.valueOf(rightAnswer));
          topRightButton.setText(String.valueOf(answers[1]));
-         midLeftButton.setText(String.valueOf(answers[2]));
-         midRightButton.setText(String.valueOf(answers[3]));
-         bottomLeftButton.setText(String.valueOf(answers[4]));
-         bottomRightButton.setText(String.valueOf(answers[5]));
+         bottomLeftButton.setText(String.valueOf(answers[2]));
+         bottomRightButton.setText(String.valueOf(answers[3]));
         }
     }
